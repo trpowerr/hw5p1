a35efd3f147393730936a9d642ba2f534cc11c22
trpowerr <48128809+trpowerr@users.noreply.github.com>
2019-10-26 22:44:00 +0300

    Update trycash.test.js

--- src/js/__tests__/trycash.test.js
+++ src/js/__tests__/trycash.test.js
@@ -1,9 +1,17 @@
 import converter from '../trycash';
 
-test('should ', () => {
-  const received = converter();
+test('should check value', () => {
+  const received = converter(10);
 
-  const expected = +converter();
+  const expected = +converter(10);
 
   expect(received).toBe(expected);
 });
+
+// test('should check value', () => {
+//   const received = converter('ff');
+
+//   const expected = 'Неверный формат ввода!';
+
+//   expect(received).toBe(expected);
+// });
